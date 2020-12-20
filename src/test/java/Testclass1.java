ae4b008ea9cdbb15f7429d0e44d08a0179f72dde
snehal20 <telangi.snehal@gmail.com>
2020-12-20 11:05:21 +0530

    Git PR Demo

---
+++ src/test/java/Testclass1.java
@@ -0,0 +1,30 @@
+
+public class Testclass1 
+{
+public void test01()
+{
+	System.out.println("i am a test 1 method");
+}
+
+public void test02()
+{
+	System.out.println(" i am test 2 method");
+	
+}
+
+public void test03()
+{
+	System.out.println(" i am test 3 method");
+	
+}
+public void test04()
+{
+	System.out.println(" i am test 4 method");
+	
+}
+public void test05()
+{
+	System.out.println(" i am test 5 method");
+	
+}
+}
---
+++ src/test/java/Testclass2.java
@@ -0,0 +1,32 @@
+
+public class Testclass2 
+{
+	public void method_01()
+	{
+		System.out.println("i am a test 1 method");
+	}
+
+	public void method_02()
+	{
+		System.out.println(" i am test 2 method");
+		
+	}
+
+	public void method_03()
+	{
+		System.out.println(" i am test 3 method");
+		
+	}
+	public void method_04()
+	{
+		System.out.println(" i am test 4 method");
+		
+	}
+	public void method_05()
+	{
+		System.out.println(" i am test 5 method");
+		
+	}
+	}
+
+
